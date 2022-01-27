81a0630291a3f140856aab097475f80dce11beaf
Alexander Suvorov <yourmedstat@gmail.com>
2022-01-27 18:16:40 +0300

    111

--- docs/pages/metaanalysis/meta.r
+++ docs/pages/metaanalysis/meta.r
@@ -1,3 +1,46 @@
-3+2
+"""
+К статье по метаанализам
 
 
+
+"""
+
+library(epiR)
+library(pwr)
+
+# Размер эффекта
+
+# График Стандартной ошибки
+
+SE = c()
+mu = c()
+
+for (i in seq(500)) {
+  sample <- rnorm(n = i, mean = 0, sd = 3)
+  SE[i] = sd(sample)/sqrt(length(sample))
+  mu[i] =  mean(sample)
+}
+
+plot(mu,xlab = 'Sample size', ylab = 'Sample mean', type="l")
+
+plot(SE,xlab = 'Sample size', ylab = 'Standard error', type="l")
+
+# standartise demo
+
+m1 =  abs(-6)/2
+m2 = (-14)/10
+sd1 = sqrt(2*(1-0.5)/n1 + (m1)/(2*n1))
+sd2 = sqrt(2*(1-0.5)/n2 + (m2)/(2*n2))
+
+sd1r = sqrt((2^2 + 2^2 - 2*(0.5 * 2*2))/n1)
+sd2r = sqrt((10^2 + 16^2 - 2*(0.5 * 10*16))/n2)
+
+n1 = 58
+n2 = 82
+
+-14 - sd2r
+
+-m1 - 2*sd1
+m2 + 2*sd2
+
+pooled_sd = sqrt(((n1-1) * sd1^2 + (n2-1) * sd2^2)/(n1 + n2 - 2))
